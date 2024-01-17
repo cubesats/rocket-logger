75c2f74dbf9118f58c78fad1d340171bf583ecb1
Luis <luis.cormier@nottingham.ac.uk>
2024-01-17 15:59:24 +0100

    Initial commit

---
+++ code.py
@@ -0,0 +1,25 @@
+import time
+import board
+import busio
+import adafruit_icm20x
+
+i2c = busio.I2C(board.GP5, board.GP4)
+icm = adafruit_icm20x.ICM20948(i2c, 0x68)
+
+buffer = ""
+
+with open("data.csv", "a") as f:
+    f.write("t_sec,acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z,magno_x,magno_y,magno_z\n")
+    i = 0
+    while True:
+
+        # Save to the buffer
+        buffer += "%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f\n" % (time.monotonic(), icm.acceleration[0], icm.acceleration[1], icm.acceleration[2], icm.gyro[0], icm.gyro[1], icm.gyro[2], icm.magnetic[0], icm.magnetic[1], icm.magnetic[2])
+
+        # Write to the flash at a slower rate (1/20 speed)
+        if(int(time.monotonic()) % 5 == 0):
+            f.write(buffer)
+            buffer = ""
+            f.flush()
+
+        time.sleep(0.1)
